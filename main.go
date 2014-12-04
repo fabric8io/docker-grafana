59e7c207bb457c5ec76cdb30a7232b88df8b4b87
Jimmi Dyson <jimmidyson@gmail.com>
2014-12-04 10:54:49 +0000

    Fix environment variables in template & add ability to specify username & password

--- main.go
+++ main.go
@@ -4,17 +4,34 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"text/template"
 )
 
+type Context struct {
+}
+
+func (c *Context) Env() map[string]string {
+	env := make(map[string]string)
+	for _, i := range os.Environ() {
+		sep := strings.Index(i, "=")
+		env[i[0:sep]] = i[sep+1:]
+	}
+	return env
+}
+
 func main() {
+	os.Setenv("INFLUXDB_URL", os.ExpandEnv(os.Getenv("INFLUXDB_URL")))
+
+	log.Println("Using ", os.Getenv("INFLUXDB_URL"))
+
 	t, _ := template.ParseFiles("config.js.tmpl")
 
-	file, err := os.Create("config.js") // For read access.
+	file, err := os.Create("config.js")
 	if err != nil {
 		log.Fatal(err)
 	}
-	t.Execute(file, nil)
+	t.Execute(file, &Context{})
 	file.Close()
 
 	fs := http.FileServer(http.Dir("."))
