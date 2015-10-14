37f5ea1673156861f2abb37aa88f5328b2ef130d
Jimmi Dyson <jimmidyson@gmail.com>
2015-10-14 14:58:04 +0100

    Preconfigured dashboards

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,17 @@
-FROM centos:7
+FROM grafana/grafana:develop
 MAINTAINER Jimmi Dyson <jimmidyson@gmail.com>
-ENTRYPOINT ["/usr/sbin/grafana-server", "--homepath=/usr/share/grafana", "--config=/etc/grafana/grafana.ini"]
-EXPOSE 3000
 
-ENV GRAFANA_VERSION 2.1.3
-ENV GF_LOG_MODE console
-ENV GF_PATHS_DATA /var/lib/grafana
-ENV GF_PATHS_LOGS /var/lib/grafana
+ENTRYPOINT ["/run.sh"]
+
 ENV GF_AUTH_ANONYMOUS_ENABLED true
 
-RUN rpm --import https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana && \
-    yum install -y https://grafanarel.s3.amazonaws.com/builds/grafana-${GRAFANA_VERSION}-1.x86_64.rpm && \
-    sed -i 's/mode.*=.*$/log.mode = console/' /usr/share/grafana/conf/defaults.ini && \
-    chmod 777 /var/lib/grafana && \
-    curl -L https://github.com/grafana/grafana-plugins/archive/master.tar.gz | tar xvz -C /usr/share/grafana/public/app/plugins/datasource/ --strip-components=2 grafana-plugins-master/datasources/prometheus
+RUN apt-get update && \
+    apt-get install -y curl && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* && \
+    chmod 777 /var/lib/grafana
 
-VOLUME /var/lib/grafana
+ADD run.sh /run.sh
+ADD dashboards /dashboards
+
 USER grafana
---
+++ run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+pid=0
+
+term_handler() {
+  pkill -TERM $pid
+  exit
+}
+
+trap 'term_handler' SIGINT
+
+HEADER_CONTENT_TYPE="Content-Type: application/json"
+HEADER_ACCEPT="Accept: application/json"
+
+GRAFANA_USER=${GRAFANA_USER:-admin}
+GRAFANA_PASSWD=${GRAFANA_PASSWD:-admin}
+
+PROMETHEUS_ADDRESS=${PROMETHEUS_ADDRESS:-'http://prometheus:9090'}
+
+DASHBOARD_LOCATION=${DASHBOARD_LOCATION:-'/dashboards'}
+
+echo "Starting Grafana in the background"
+/usr/sbin/grafana-server \
+  --homepath=/usr/share/grafana \
+  config=/etc/grafana/grafana.ini \
+  cfg:default.paths.data=/var/lib/grafana \
+  cfg:default.paths.logs=/var/log/grafana &
+pid=$!
+
+echo "Waiting for Grafana to start..."
+until $(curl --fail --output /dev/null --silent http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:3000/api/org); do
+  printf "."
+  sleep 1
+done
+echo "Grafana is up and running."
+
+if [ ! -f /var/lib/grafana/.configured ]; then
+  echo "Creating default prometheus datasource..."
+  curl -i -XPOST -H "${HEADER_ACCEPT}" -H "${HEADER_CONTENT_TYPE}" "http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:3000/api/datasources" -d '
+  { 
+    "name": "kubernetes-prometheus",
+    "type": "prometheus",
+    "access": "proxy",
+    "isDefault": true,
+    "url": "'"${PROMETHEUS_ADDRESS}"'"
+  }'
+
+  echo ""
+  echo "Importing default dashboards..."
+  for filename in ${DASHBOARD_LOCATION}/*.json; do
+    echo "Importing ${filename} ..."
+    curl -sS -i -XPOST -d "{\"dashboard\":`cat ${filename}`,\"overwrite\": true}" -H "${HEADER_ACCEPT}" -H "${HEADER_CONTENT_TYPE}" "http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:3000/api/dashboards/db"
+    echo ""
+    echo "Done importing ${filename}"
+  done
+  echo ""
+fi
+
+wait $pid
